7033f7d6a5774e8b6b0cb710c506fba11f99526c
pauln <paulnicolai.hennings@gmail.com>
2023-03-02 18:04:14 +0100

    feat: prepared track history

---
+++ extensions/music/m_back.py
@@ -0,0 +1,64 @@
+import logging
+
+import discord
+from discord.ext import commands
+import pomice
+from pycord import multicog
+
+from DragonPlayer.DragonPlayer import DragonPlayer
+from DragonBot import DragonBot
+from utils import db
+
+
+class TrackBackCog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @multicog.add_to_group("music")
+    @commands.slash_command(name = "back", description = "Play the previous song")
+    async def track_back(self, ctx: discord.ApplicationContext):
+        if not ctx.voice_client:
+            return await ctx.response.send_message(
+                "You can't go back a track if I am not playing",
+                ephemeral=True,
+                delete_after=10,
+            )
+        player: DragonPlayer = ctx.voice_client
+        if (
+            ctx.author.voice is None
+            or ctx.author.voice.channel != ctx.voice_client.channel
+        ):
+            return await ctx.response.send_message(
+                f"You may not go back a track when you are not in the same channel.",
+                ephemeral=True,
+                delete_after=10,
+            )
+
+        if len(player.queue.history) == 0:
+            await ctx.response.send_message(
+                f"No song in history!", ephemeral = True, delete_after = 10
+            )
+
+        current = player.current
+        track = player.queue.history[:-1]
+        player.queue.history.pop(-1)
+        player.queue.put(track)
+        await player.do_next()
+        player.queue.put(current)
+
+        await ctx.response.send_message(
+            f"You went back to {track}", ephemeral = True, delete_after = 10
+        )
+        controller: discord.Message = player.controller
+        await controller.channel.send(
+            embed = discord.Embed(color = discord.Color.blurple()).set_author(
+                name = f"{ctx.author.display_name} went back to {track}",
+                icon_url = ctx.author.display_avatar.url,
+            ),
+            delete_after = 10,
+        )
+# TODO: Check if Track fails
+
+
+def setup(client: DragonBot):
+    client.add_cog(TrackBackCog(client))
