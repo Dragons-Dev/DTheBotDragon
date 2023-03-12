955e9880c398787408440a6b651f33d041d18442
pauln <paulnicolai.hennings@gmail.com>
2023-03-12 21:37:53 +0100

    dev: start development of a economy system

---
+++ extensions/fun/e_bank.py
@@ -0,0 +1,22 @@
+import logging
+
+import discord
+from discord.ext import commands
+
+from DragonBot import DragonBot
+from utils import db
+
+
+class EcoBankCog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command("bank")
+    async def add_bank(self, ctx: commands.Context):
+        for member in ctx.guild.members:
+            m = await db.get_bank_acc(member.id)
+            print(f"{member.name} got: {m[0]}€ | {m[1]}USD | {m[2]} Gold | {m[3]} Rolex")
+
+
+def setup(client: DragonBot):
+    client.add_cog(EcoBankCog(client))
